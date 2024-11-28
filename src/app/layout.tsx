e6c47ab39a998e76c43ec2c7a6e3bc04ad5ca911
Rizqi Nur Andi Putra <e31211961@student.polije.ac.id>
2024-11-28 10:43:50 +0700

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: 'Coding Service',
+  title: 'Codingin',
   description: 'Professional Web Development Services',
 }
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
