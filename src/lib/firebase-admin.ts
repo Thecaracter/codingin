a0948ba9ab1a995704725a245f157a24866db236
Rizqi Nur Andi Putra <e31211961@student.polije.ac.id>
2024-12-05 07:33:11 +0700

    /

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,12 +3,10 @@ import { getMessaging } from 'firebase-admin/messaging';
 import prisma from "@/lib/prisma";
 
 if (!getApps().length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
+
     initializeApp({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
+        credential: cert(serviceAccount)
     });
 }
 
